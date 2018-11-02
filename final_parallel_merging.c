9ec653770e0124b6ded284312e94c812becbd150
SteveMM <31938056+SteveMM@users.noreply.github.com>
2018-11-02 16:50:54 -0400

    Add files via upload

--- final_parallel_merging.c
+++ final_parallel_merging.c
@@ -11,13 +11,14 @@
 #include <mpi.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int generate_array(char * filename, long size){
       
     FILE *f;
     
-    f = fopen(fileName,"w");
+    f = fopen(filename,"w");
     
     if (f==NULL){
         printf("file can't be opened for writing\n");
@@ -86,11 +87,11 @@ long binary_search(FILE *file, long n, long target){
  *Output:
  *		-the array passed as C will be modified as merged A & B
 */
-long serial_merge(long *a, long *b, long *c, long a_start, long a_end, long b_start, long b_end){
-	long i=a_start;
-	long j=b_start;
-	long k=a_start+b_start;
-	long n=a_end-a_start+b_end-b_start;
+long serial_merge(long *a, long *b, long *c, long sizea, long sizeb){
+	long i=0;
+	long j=0;
+	long k=0;
+	long n=sizea+sizeb;
 	c=malloc(sizeof(long)*n);
 	while (k<n){
 		long a_i=a[i];
@@ -109,7 +110,7 @@ long serial_merge(long *a, long *b, long *c, long a_start, long a_end, long b_st
 }
 
 long load_array(FILE *file, long start, long end, long *a){
-	size = end - start;
+	long size = end - start;
 	a=malloc(sizeof(long)*(size));
 	fseek(file,start,SEEK_SET);
 	fread(a,4,size,file);
@@ -120,26 +121,64 @@ long load_array(FILE *file, long start, long end, long *a){
 	
 }
 
-long main(long argc, char **argv){
+
+void write_log (char *text ){
+    FILE *logf;
+    logf = fopen ("log1.txt", "a");
+    
+    fwrite (text, 1, strlen(text),logf);
+    
+    fwrite ("\n", 1, 1,logf);
+    
+    fclose(logf);
+    
+}
+
+long main(int argc, char **argv){
 	char *file1;
 	char *file2;
 	char *file3;
+    
+    
+    
+    
+     write_log("started main");
 	
-	file1=argv[0];
-	file2=argv[2];
-	file3=argv[4];
+    MPI_Status status;
+	
+    
+   
+    
+    file1=argv[1];
+	file2=argv[3];
+	file3=argv[5];
+    
+    
+  
 
-	long n1=argv[1];
-	long n2=argv[3];
-	generate_array(file1, n1);
-	generate_array(file2, n2);
+	long n1=atol(argv[2]);
+	long n2=atol(argv[4]);
+    
+    write_log( " parsed inputs");
+     
+    
+	
+    generate_array(file1, n1);
+	 write_log("generated 1");
+    
+    
+    generate_array(file2, n2);
+      write_log("generated 2");
+    
+    
+    
 	
 	MPI_Init(&argc, &argv);
 	int id,p;
 	long *a;
 	long *b;
 	long *c;
-	long *bigC:
+	long *bigC;
 	long n3 = n1+n2;
 	FILE  *f1 =NULL;
 	FILE  *f2 =NULL;
@@ -160,48 +199,75 @@ long main(long argc, char **argv){
 	//1cs+1 2cs
 	//2cs+1 3cs
 	if (id!=p-1){
-		i_0=id*cs;
+		i_0=id*chunk_size;
 		i_1=(id+1)*chunk_size;
+        
+        
+            write_log("decided on indexes for first array");
+     
 		size_a=chunk_size;
-		load_array(file1, i_0,i_1, a);
+		load_array(f1, i_0,i_1, a);
 		
-		j_0=binary_search(file2,n2,i_0);
-		j_1=binary_search(file2,n2,i_1);
+		j_0=binary_search(f2,n2,i_0);
+		j_1=binary_search(f2,n2,i_1);
 		
-		b_size = load_array(file2, j_0,j_1,b);
-		serial_merge(a,b,c,0,chunk_size,0, b_size);
-		
+        write_log("decided on indexes for 2nd array");
+        
+        
+		b_size = load_array(f2, j_0,j_1,b);
+        
+        write_log("merging chunk");
+		serial_merge(a,b,c,chunk_size, b_size);
+        write_log("merged chunk");
+        
 	}else{
-		i_0=id*cs;
+		i_0=id*chunk_size;
 		i_1=n1-1;
 		size_a=i_1-i_0;
-		load_array(file1, i_0,i_1, a);		
-		j_0=binary_search(file2,n2,i_0);
+        
+        write_log("decided on indexes for first array");
+       
+        
+
+		load_array(f1, i_0,i_1, a);		
+		j_0=binary_search(f2,n2,i_0);
 		j_1=n2-1;
-		b_size = load_array(file2, j_0,j_1,b);
-		serial_merge(a,b,c,0,size_a,0,b_size);
+        
+        write_log("decided on indexes for 2nd array");
+		b_size = load_array(f2, j_0,j_1,b);
+        write_log("merging chunk");
+		serial_merge(a,b,c,size_a,b_size);
+        write_log("merged chunk");
 	}
 	
 	if (id==0){
+        write_log ("receiving");
 		bigC=malloc(sizeof(long)*n3);
 		long offset = 0;
-		for(source=1;source<p;source++){
+		for(int source=1;source<p;source++){
+            write_log ("receiving proc");
+            
+          
 			long rcv_size;
 			MPI_Recv(&rcv_size,1,MPI_UNSIGNED_LONG,source,1,MPI_COMM_WORLD,&status);
 			MPI_Recv(bigC+offset,rcv_size,MPI_UNSIGNED_LONG,source,2,MPI_COMM_WORLD,&status);
 			offset+=rcv_size;
 		}
+        write_log ("received all");
 		FILE *f;
 		f = fopen(file3,"w");
 		fwrite(bigC,4,offset,f);
 		fclose(f);
 
 	}else{
+        write_log ("sending"); 
 		MPI_Send(&size_a,1,MPI_UNSIGNED_LONG,0,1,MPI_COMM_WORLD);
 		MPI_Send(c,1,MPI_UNSIGNED_LONG,0,2,MPI_COMM_WORLD);	
+        write_log ("sent");
 	}
 
 	MPI_Finalize();
+    return 0;
 	}
-	return 0;
-}
+	
+
